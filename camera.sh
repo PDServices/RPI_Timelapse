3278a1a1c3271fbba4cf4bd12daca287a1bc5b61
Paul Spencer <pauldsp@outlook.com>
2017-08-28 15:34:11 +0100

    Reformatted header

--- camera.sh
+++ camera.sh
@@ -1,36 +1,36 @@
-#!/bin/bash																		#
-#===============================================================================#
-# HEADER																		#
-#===============================================================================#
-# SCRIPT:		Camera.sh 														#
-# AUTHOR:		Paul Spencer													#
-# DATE:			24/08/2016														#
-# COPYRIGHT:	Copyright (c) Paul Spencer		 								#
-# LICENCE:		MIT License														#
-#																				#
-# PURPOSE: This is a simple loop to take a timelapse series of pictures			#
-#			with the RPI Camera													#
-#																				#
-#===============================================================================#
-# HISTORY																		#
-#===============================================================================#
-#	Date		Author	Description												#
-# 	24/08/2016	PDS		Script creation 										#
-#	09/06/2017	PDS		Added std Header 										#
-# 																				#
-#===============================================================================#
-# USAGE																			#
-#===============================================================================#
-# Comment out if not needed														#
+#!/bin/bash
+##########
+# HEADER
+##########
+# SCRIPT:		Camera.sh
+# AUTHOR:		Paul Spencer
+# DATE:			24/08/2016
+# COPYRIGHT:	Copyright (c) Paul Spencer
+# LICENCE:		MIT License
+#
+# PURPOSE: This is a simple loop to take a timelapse series of pictures
+#			with the RPI Camera
+#
+##########
+# HISTORY
+##########
+#	Date		Author	Description
+# 	24/08/2016	PDS		Script creation
+#	09/06/2017	PDS		Added std Header
+#
+##########
+# USAGE
+##########
+# Comment out if not needed
 # USAGE="Please provide end date in format yyyy-mm-dd_hhmmss"
 
 # if [ $# == 0 ] ; then
 #     echo $USAGE
 #     exit 1;
 # fi
-#===============================================================================#
-# END_OF_HEADER																	#
-#===============================================================================#
+##########
+# END_OF_HEADER
+##########
 DELAY=30
 
 while [ 1 = 1 ]
