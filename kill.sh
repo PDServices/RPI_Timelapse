075c4869b4384aff9d3cf1059114e8ac97009068
Paul Spencer <paul@pdsupport.net>
2019-04-01 21:56:17 +0100

    Included photo's directory, tweaked camera.sh, include basic kill.sh. added *.iml to ignore
    Signed-off-by: Paul Spencer <pauldsp@outlook.com>

--- kill.sh
+++ kill.sh
@@ -1,3 +1,26 @@
-PID=`ps -ef | grep camera.sh | grep -v grep | awk '{print$2}'`
+#!/bin/bash
+
+##################################################
+# HEADER
+##################################################
+# SCRIPT:		PID Kill script
+# AUTHOR:		Paul Spencer
+# DATE:			01/04/2019
+#
+# PURPOSE: This script identifies a PID of a given process
+#           and kill's it
+#
+##################################################
+# HISTORY
+##################################################
+#	Date		Author	Description
+# 	01/04/2019	PDS		Script creation
+#
+##################################################
+# END_OF_HEADER
+##################################################
+
+PROCESS=camera.sh
+PID=`ps -ef | grep ${PROCESS} | grep -v grep | awk '{print$2}'`
 
 kill -9 $PID
